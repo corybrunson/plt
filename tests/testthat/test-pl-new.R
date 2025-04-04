ad8eed0c7e7bdc8f6729daa3aed2d5cf93cced3a
James Golabek <jamesgolabek@gmail.com>
2025-04-04 01:27:08 -0400

    Add unit tests for pl_is_exact and pl_limits

--- tests/testthat/test-pl-new.R
+++ tests/testthat/test-pl-new.R
@@ -39,3 +39,15 @@ test_that("`pl_support()` works", {
   expect_no_error(pl_support(pl_new(pd_a,degree = 1)))
   expect_no_error(pl_support(pl_new(pd_r,degree = 1)))
 })
+
+test_that("`pl_is_exact()` works", {
+  expect_equal(pl_is_exact(pl_new(pd_a,degree = 1)),
+               pl_is_exact(pl_new(pd_r,degree = 1)))
+})
+
+test_that("`pl_limits()` works", {
+  expect_no_error(pl_limits(pl_new(pd_a,degree = 1)))
+  expect_no_error(pl_limits(pl_new(pd_r,degree = 1)))
+  expect_equal(pl_limits(pl_new(pd_r,degree = 1))[2],
+               pl_support(pl_new(pd_r,degree = 1))[2])
+})
