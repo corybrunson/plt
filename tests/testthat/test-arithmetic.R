e6e75372b926ab8ddd0a3041aa218ef4b4d6c505
a1danbryant <bryant.aidan15@gmail.com>
2025-04-07 14:08:25 +0900

    unit tests for  and

--- tests/testthat/test-arithmetic.R
+++ tests/testthat/test-arithmetic.R
@@ -2,6 +2,9 @@
 # tiny example
 pd <- cbind(start = 2, end = 5)
 pl <- pl_new(pd, exact = TRUE)
+#larger example
+pd2 <- matrix(c(0, 1, .25, 1.25), ncol = 2, byrow = TRUE)
+pl2 <- pl_new(pd2, exact = False)
 
 test_that("singletons are handled as lists", {
   # sum
@@ -43,9 +46,26 @@ test_that("vectorizations work",{
 })
 
 test_that("`pl_abs` correctly returns the absolute value of a PL", {
-  pl2 <- -1 * pl
-  pl3 <- pl_abs(pl2)
+  pl3 <- -1 * pl
+  pl4 <- pl_abs(pl3)
   
-  expect_equal(pl_distance(pl, pl3), 0) # Note expect equal test wont work on the PLs themselves, why?
+  expect_equal(pl_distance(pl, pl4), 0) # Note expect equal test wont work on the PLs themselves, why?
   
+})
+
+test_that("`pl_add` behaves as expected", {
+  pla <- pl2 + pl2
+  plb <- pl_add(pl2, pl2)
+  plc <- 2 * pl2
+  pls <- list(pla, plb, pl)c
+  expect_equal(sum(pl_dist(pls)), 0)
+})
+
+test_that("`pl_inner` obeys: symmetrical, nonnegative, and linearity ", {
+  pl1 <- -1 * pl
+  pla <- .75 * pl2
+  
+  expect_equal(pl_inner(pl1, pl2), pl_inner(pl2, pl1))
+  expect_gte(pl_inner(pl2, pl2), 0)
+  expect_equal(.75 * pl_inner(pl2, pl1), pl_inner(pla, pl1))
 })
