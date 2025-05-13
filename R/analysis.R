43b5c628207031f6b490e24e9cc6c1c5d43c00c4
a1danb <124316415+a1danbryant@users.noreply.github.com>
2025-05-13 11:22:46 +0900

    Update to details of analysis.R

--- R/analysis.R
+++ R/analysis.R
@@ -1,7 +1,28 @@
 #' @title Integrals, Distances, and Norms of Persistence Landscapes
 #' @description Calculate distances and perform infinitesimal calculus on
 #'   persistence landscapes. See Section 3 of Bubenik (2015).
+#' @details `pl_integrate()` computes the \eqn{L^p} integral of a persistence
+#'   landscape. The `p` parameter denotes the integral's power;
+#'   `p = 1` computes the area under the curves, while `p = 2` corresponds to the
+#'    \eqn{L^2} norm. Letting `p = Inf` returns the maximum landscape height.
 #'
+#'   `pl_distance()` calculates the \eqn{L^p} distance between two persistence
+#'   landscapes. 
+#'
+#'   `pl_dist()` utilizes `pl_distance()` to compute all pairwise distances
+#'   between a list of landscapes. It returns a symmetric matrix of distances.
+#'
+#'   `pl_norm()` returns the \eqn{L^p} norm of a single landscape, used as a
+#'   scalar summary of its magnitude. Similar to `pl_integrate()`, it supports
+#'   all finite powers `p >= 1`, as well as `p = Inf` for the supremum norm.
+#'
+#'   `pl_indicator()` computes the linear form defined by a sum of indicator
+#'   functions over user-specified support intervals, weighted by \eqn{1/k^r}
+#'   where \eqn{k} indexes the landscape levels.
+#'
+#'   `pl_indicator_form()` evaluates the integral of the indicator function form
+#'   described above with respect to the \eqn{L^p} norm. It returns a real number
+#'   representing the landscape's integral onto the chosen intervals.
 #' @name analysis
 #' @include arithmetic.R
 #' @inheritParams pl_new
