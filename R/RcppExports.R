6451c566ed6e41e5fd348a4fb336f793d4484299
Jason Cory Brunson <cornelioid@gmail.com>
2025-05-14 10:59:57 -0400

    rebuild site

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -9,7 +9,7 @@
 #' @details The C++ class 'PersistenceLandscape' is exposed as the S4 class
 #'   'Rcpp_PersistenceLandscape' via the `RCPP_MODULE()` macro provided by
 #'   **[Rcpp][Rcpp::Rcpp-package]**. See
-#'   <https://github.com/coatless-r-n-d/rcpp-modules-student> for an
+#'   <https://github.com/coatless-rd-rcpp/rcpp-modules-student> for an
 #'   introduction. New objects should be created from persistence data
 #'   (diagrams) using [pl_new()].
 #' @field new Constructor.
